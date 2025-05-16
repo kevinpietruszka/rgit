a709489b47864a2de7125ac352c61982d3eb07ac
Kevin Pietruszka <kevin.pietruszka@proton.me>
2025-05-15 20:29:34 -0400

    adds check for repo already initialized
    Gives a warning if the the rgit directory already exists. If so stops
    the creation of the reposotory.

--- src/subcommand/init.rs
+++ src/subcommand/init.rs
@@ -2,6 +2,7 @@ use crate::command::RgitCommand;
 use crate::constants::{RGIT_DIR};
 use crate::result::RgitResult;
 use std::fs;
+use std::path;
 
 pub struct InitCommand {}
 
@@ -13,6 +14,9 @@ impl InitCommand {
 
 impl RgitCommand for InitCommand {
     fn run(&self) -> RgitResult {
+        if path::Path::new(RGIT_DIR).exists() {
+            return RgitResult::Warning(format!("Repository already exists in {} directory", RGIT_DIR));
+        }
         match fs::create_dir(RGIT_DIR) {
             Ok(()) => RgitResult::Success(format!("Created repository in {} directory", RGIT_DIR)),
             Err(e) => RgitResult::Fatal(format!("Could not create the repository with error ({})", e)),
