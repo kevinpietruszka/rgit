71929486e5e43f273ad463cf32f0bc9eadd2592d
Kevin Pietruszka <kevin.pietruszka@proton.me>
2025-06-07 16:25:30 -0400

    setup creating sub directories

--- src/constants.rs
+++ src/constants.rs
@@ -1,1 +1,4 @@
 pub const RGIT_DIR: &str = ".rgit";
+
+pub const OBJECT_DIR: &str = ".rgit/objects";
+pub const SUB_DIRS: [&str; 1] = [OBJECT_DIR];
--- src/subcommand/init.rs
+++ src/subcommand/init.rs
@@ -1,5 +1,5 @@
 use crate::command::RgitCommand;
-use crate::constants::{RGIT_DIR};
+use crate::constants::{RGIT_DIR,SUB_DIRS};
 use crate::result::RgitResult;
 use std::fs;
 use std::path;
@@ -18,8 +18,23 @@ impl RgitCommand for InitCommand {
             return RgitResult::Warning(format!("Repository already exists in {} directory", RGIT_DIR));
         }
         match fs::create_dir(RGIT_DIR) {
-            Ok(()) => RgitResult::Success(format!("Created repository in {} directory", RGIT_DIR)),
-            Err(e) => RgitResult::Fatal(format!("Could not create the repository with error ({})", e)),
+            Ok(()) => (),
+            Err(e) => return RgitResult::Fatal(format!("Could not create the repository with error ({})", e)),
         }
+
+        let mut errors = vec![];
+
+        for dir in SUB_DIRS {
+            match fs::create_dir(dir) {
+                Ok(()) => (),
+                Err(e) => errors.push(format!("\nFailed to create {} subdir with error {}", dir, e)),
+            }
+        }
+
+        if errors.len() > 0 {
+            return RgitResult::Warning(format!("Repository created with warnings:{}", errors.join("")));
+        }
+
+        RgitResult::Success(format!("Created repository in {} directory", RGIT_DIR))
     }
 }
